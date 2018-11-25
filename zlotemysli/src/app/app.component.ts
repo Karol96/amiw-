a0ab9010f24b57908a5d0cde4c0b869fb0046f8f
Karol96 <stempniu96@o2.pl>
2018-11-25 19:14:28 +0100

    1

---
+++ zlotemysli/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public text: String = "szczerozłota myśl"
+  public thoughts: Array<String> = []
+
+  public addThought(): void {
+    this.thoughts.push(this.text)
+	console.log(this.thoughts)
+	this.text = ""
+  }
+  public removeThought(i): void {
+  this.thoughts.splice(i, 1)
+}
+}
+
+
